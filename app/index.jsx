25f874c48cb2b79ccfddb871740031d473beb5b8
Teo Mou Qi <kimberlyteomq@gmail.com>
2024-05-27 14:47:11 +0800

    Changes to be committed: new file:   app/(auth)/_layout.jsx new file:   app/(auth)/log-in.jsx new file:   app/(auth)/sign-up.jsx new file:   app/(tabs)/_layout.jsx new file:   app/(tabs)/camera.jsx new file:   app/(tabs)/home.jsx new file:   app/(tabs)/index.jsx new file:   app/(tabs)/map.jsx new file:   app/(tabs)/search.jsx modified:   app/index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Hello!</Text>
+      {/*using this just to see home and the tabs for now, can rm later*/}
+      <Link href="/home" style={styles.container}>Go to Home</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
