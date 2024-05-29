46ecb62fa9af2b8c25d0192da37319e64e18be15
Amelia Chow <ameliaaaa204@macbook-air-15.lan>
2024-05-29 16:29:22 +0800

    connecting login to home

--- app/(tabs)/search.jsx
+++ app/(tabs)/search.jsx
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 import React from 'react'
+import { FIREBASE_AUTH} from '../../FirebaseConfig'; 
 
-const Search = () => {
+const Search = ({ navigation }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Search</Text>
+      <Button onPress={() => FIREBASE_AUTH.signOut()} title="Logout" />
     </View>
   )
 }
 
 export default Search
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1
+  }
+});
