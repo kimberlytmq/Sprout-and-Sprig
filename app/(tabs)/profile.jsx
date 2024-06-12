49c8eca7b7c1e86d3d5316f513d17e35db848a21
kimberlytmq <104084634+kimberlytmq@users.noreply.github.com>
2024-06-13 02:09:21 +0800

    Profile Page UI modified:   app/(tabs)/_layout.jsx modified:   app/(tabs)/profile.jsx modified:   app/(tabs)/search.jsx

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,8 +1,10 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { db } from '../../FirebaseConfig'
 import { doc, getDoc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { BottomTabBarHeightCallbackContext } from '@react-navigation/bottom-tabs';
+import { useAuth } from '../../context/authContext';
 
 
 
@@ -30,9 +32,25 @@ const Profile = () => {
 
     fetchUsername();
   }, [user]);
+
+
+  const { logout } = useAuth();
+  const handleLogout = async ()=>{
+    await logout();
+  }
+  
+
   return (
     <View style={styles.container}>
-      <Text>Username: {username}</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>Hi, {username}!</Text>
+      </View>
+      <Text style={styles.subtitle}>Achievements</Text>
+      <Text style={styles.subtitle}>Photos</Text>
+      <TouchableOpacity style={styles.button} onPress={handleLogout} >
+        <Text style={styles.buttonText} >Logout</Text>
+      </TouchableOpacity> 
+      
     </View>
   )
 }
@@ -42,9 +60,44 @@ export default Profile
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
-    alignItems: "center",
     flex: 1,
-    backgroundColor: 'white'
+    backgroundColor: "#E9F7EF",
+
+  },
+  header: {
+    height: 150,
+    paddingTop: 50,
+    backgroundColor: '#397004',
+    justifyContent: 'center',
+    alignItems : 'center'
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 35,
+    color: "white",
+  },
+  subtitle: {
+    fontWeight: "bold",
+    fontSize: 25,
+    color: '#397004',
+    marginTop: 15,
+    marginLeft: 10,
+    marginBottom: 15
+  },
+  button: {
+    backgroundColor: "#397004",
+    padding: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 100,
+    width: 100,
+    marginBottom: 15,
+    marginTop: 15,
+    marginLeft: 10
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "white"
   }
 });
