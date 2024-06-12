4fb4f95dde1ceba2d0e9fb70a76dbc55148e8303
kimberlytmq <104084634+kimberlytmq@users.noreply.github.com>
2024-06-13 01:02:36 +0800

    Profile page displays username modified:   app/(tabs)/profile.jsx

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,10 +1,38 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { db } from '../../FirebaseConfig'
+import { doc, getDoc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+
 
 const Profile = () => {
+  const [username, setUsername] = useState('');
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  useEffect(() => {
+    const fetchUsername = async () => {
+      try {
+        if (user) {
+          const userDoc = doc(db, 'users', user.uid);
+          const docSnap = await getDoc(userDoc);
+          if (docSnap.exists()) {
+            setUsername(docSnap.data().username);
+          } else {
+            console.log('No such document!');
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching document:', error);
+      }
+    };
+
+    fetchUsername();
+  }, [user]);
   return (
     <View style={styles.container}>
-      <Text>Profile</Text>
+      <Text>Username: {username}</Text>
     </View>
   )
 }
@@ -16,6 +44,7 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: "center",
     alignItems: "center",
-    flex: 1
+    flex: 1,
+    backgroundColor: 'white'
   }
 });
