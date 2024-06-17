39fe1c3979a0329ea174d9b1c84d21d9c9b57e61
kimberlytmq <104084634+kimberlytmq@users.noreply.github.com>
2024-06-17 23:07:19 +1000

    Updates and map feature modified:   app/(tabs)/map.jsx modified:   package-lock.json modified:   package.json

--- app/(tabs)/map.jsx
+++ app/(tabs)/map.jsx
@@ -1,20 +1,21 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
-const Map = () => {
+export default function Map() {
   return (
     <View style={styles.container}>
-      <Text>Map</Text>
+      <MapView style={styles.map} />
     </View>
-  )
+  );
 }
-
-export default Map
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1
-  }
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
 });
