98169ead52a2a7087287109e1853cc82968c2c6a
kimberlytmq <104084634+kimberlytmq@users.noreply.github.com>
2024-06-28 01:27:45 +0800

    Map feature - track user location modified:   app/(tabs)/map.jsx

--- app/(tabs)/map.jsx
+++ app/(tabs)/map.jsx
@@ -1,65 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 import * as Location from "expo-location";
-import { getLocaleDirection } from 'react-native-web/dist/cjs/modules/useLocale';
 
 const SINGAPORE = {
   latitude: 1.32,
   longitude: 103.84,
-  latitudeDelta: 0.5,
-  longitudeDelta: 0.5
+  latitudeDelta: 0.005,
+  longitudeDelta: 0.005
 }
 
 export default function Map() {
-  const [currentLocation, setCurrentLocation] = useState(null)
-  const [initialRegion, setInitialRegion] = useState(null)
+  const [mapRegion, setMapRegion] = useState(SINGAPORE)
+
+  const userLocation = async () => {
+    let {status} = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied')
+    }
+    let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true})
+    setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.005,
+      longitudeDelta: 0.005
+    })
+    console.log(location.coords.latitude, location.coords.longitude)
+  }
 
   useEffect(() => {
-    const getLocation = async () => {
-      const { permission } = await Location.requestForegroundPermissionsAsync();
-      if (permission != "granted") {
-        console.log("Permission to access location not given");
-        return;
-      }
-
-      try {
-        let location = await Location.getCurrentPositionAsync({})
-        setCurrentLocation(location.coords)
-
-        setInitialRegion({
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-          latitudeDelta: 0.005,
-          longitudeDelta: 0.005
-        })
-      } catch (error) {
-        console.log('Error fetching location:', error)
-      }
-    }
-
-    getLocation();
-  },[])
-
+    userLocation()
+  }, [])
   return (
     <View style={styles.container}>
-      {initialRegion ? (
-        <MapView style={styles.map} initialRegion={initialRegion}>
-        {currentLocation && (
-          <Marker
-            coordinate={{
-              latitude: currentLocation.latitude,
-              longitude: currentLocation.longitude,
-            }}
-            title="Your Location"
-            />
-        )}
-        </MapView>
-      ) : (
-        <View style={styles.map} />
-      )}
+      <MapView 
+      style={styles.map}
+      region={mapRegion} 
+      >
+        <Marker coordinate={mapRegion} title='Marker'/>
+      </MapView>
+      <Button title='Get Location' onPress={userLocation}/>
     </View>
-  );
+  )
+  
 }
 
 const styles = StyleSheet.create({
